ee0fe77e568a0c1fae1aff37cd2c0711c01a49ff
Aaronqcd <812159920@qq.com>
2015-10-07 23:40:45 +0800

    修改路径

--- resources/views/seg/head.blade.php
+++ resources/views/seg/head.blade.php
@@ -18,34 +18,34 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <!-- Favicon -->
-    <link rel="shortcut icon" href="/assets/img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="{{base_url()}}assets/img/favicon.ico" type="image/x-icon">
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="/assets/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{base_url()}}assets/plugins/bootstrap/css/bootstrap.min.css">
     <!-- Font Icons -->
-    <link rel="stylesheet" href="/assets/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/node_modules/toastr/build/toastr.min.css">
-    {{--<link rel="stylesheet" href="/assets/css/simple-line-icons.css">--}}
+    <link rel="stylesheet" href="{{base_url()}}assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{base_url()}}node_modules/toastr/build/toastr.min.css">
+    {{--<link rel="stylesheet" href="{{base_url()}}assets/css/simple-line-icons.css">--}}
     <!-- CSS Animate -->
-    <link rel="stylesheet" href="/assets/css/animate.css">
+    <link rel="stylesheet" href="{{base_url()}}assets/css/animate.css">
     <!-- Switchery -->
-    {{--<link rel="stylesheet" href="/assets/plugins/switchery/switchery.min.css">--}}
+    {{--<link rel="stylesheet" href="{{base_url()}}assets/plugins/switchery/switchery.min.css">--}}
     <!-- Custom styles for this theme -->
-    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="{{base_url()}}assets/css/main.css">
     <!-- Vector Map  -->
-    {{--<link rel="stylesheet" href="/assets/plugins/jvectormap/css/jquery-jvectormap-1.2.2.css">--}}
+    {{--<link rel="stylesheet" href="{{base_url()}}assets/plugins/jvectormap/css/jquery-jvectormap-1.2.2.css">--}}
     <!-- ToDos  -->
-    {{--<link rel="stylesheet" href="/assets/plugins/todo/css/todos.css">--}}
+    {{--<link rel="stylesheet" href="{{base_url()}}assets/plugins/todo/css/todos.css">--}}
     <!-- Morris  -->
-    {{--<link rel="stylesheet" href="/assets/plugins/morris/css/morris.css">--}}
+    {{--<link rel="stylesheet" href="{{base_url()}}assets/plugins/morris/css/morris.css">--}}
     <link rel="stylesheet" href="{{base_url()}}css/base.css">
     <link rel="stylesheet" href="{{base_url()}}node_modules/ng-dialog/css/ngDialog.css">
     <link rel="stylesheet" href="{{base_url()}}node_modules/ng-dialog/css/ngDialog-theme-default.css">
     <!-- Fonts -->
     <!-- Feature detection -->
-    {{--<script src="/assets/js/modernizr-2.6.2.min.js"></script>--}}
+    {{--<script src="{{base_url()}}assets/js/modernizr-2.6.2.min.js"></script>--}}
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-    <!--<script src="/assets/js/html5shiv.js"></script>-->
-    <!--<script src="/assets/js/respond.min.js"></script>-->
+    <!--<script src="{{base_url()}}assets/js/html5shiv.js"></script>-->
+    <!--<script src="{{base_url()}}assets/js/respond.min.js"></script>-->
     <![endif]-->
 </head>
