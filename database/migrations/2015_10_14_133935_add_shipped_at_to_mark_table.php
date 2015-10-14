3f9ed9d76ad705f7a997ae5edadec847f624590c
followtheart <followtheart@outlook.com>
2015-10-14 13:43:12 +0800

    添加shipped_at列到mark表

---
+++ database/migrations/2015_10_14_133935_add_shipped_at_to_mark_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddShippedAtToMarkTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mark', function (Blueprint $table) {
+            //
+          $table->dateTime('shipped_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mark', function (Blueprint $table) {
+            //
+        });
+    }
+}
