4a47d46f8a1cc30fed70c5f71c88b8348abf66d3
Insane <luojieyy@gmail.com>
2015-10-17 23:53:10 +0800

    修改 dropdown

--- resources/views/seg/navbar.blade.php
+++ resources/views/seg/navbar.blade.php
@@ -161,7 +161,7 @@
                 <a class="dropdown-toggle" data-toggle="dropdown">
                     {{username()}} <i class="fa fa-angle-down"></i>
                 </a>
-                <ul class="dropdown-menu animated fadeInDown">
+                <ul class="dropdown-menu animated fadeInDown dropdown-menu-right">
                     <li>
                         <a href="" ui-sref-opts="{reload:true}" ui-sref="base.me"><i class="fa fa-user"></i>个人设置</a>
                     </li>
