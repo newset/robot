37022d5138a2c9ea7f973abf53b19f88f5593652
丁一鸣 <315129552@qq.com>
2015-10-18 20:38:11 +0800

    丁一鸣更正科室登录首页

--- resources/views/page/home/department.blade.php
+++ resources/views/page/home/department.blade.php
@@ -1,48 +1,51 @@
-<div class="row">  <h2>医院信息</h2> 
-    <hr class="home-hr"/>
-      <p>可用Mark:22个</p>   <h2>科室信息</h2> 
-    <hr class="home-hr"/>
-     
-    <table class="table table-bordered table-hover"> 
-        <tr class="active"> 
-            <th>人员</th>
-             
-            <th>本月使用</th>
-             
-            <th>未归档</th>
-             
-            <th>总计使用</th>
-             
-            <th>总计归档</th>
-             
-        </tr>
-          
-        <tr> 
-            <td>上官金红</td>
-             
-            <td>4</td>
-             
-            <td>15</td>
-             
-            <td>95</td>
-             
-            <td>80</td>
-             
-        </tr>
-          
-        <tr> 
-            <th>合计</th>
-             
-            <td>4</td>
-             
-            <td>15</td>
-             
-            <td>95</td>
-             
-            <td>80</td>
-             
-        </tr>
+<div class="container">
+    <div class="row"> 
+        <h2>医院信息</h2> 
+        <hr class="home-hr"/>
+          <p>可用Mark:22个</p>   <h2>科室信息</h2> 
+        <hr class="home-hr"/>
          
-    </table>
-     
-</div> 
+        <table class="table table-bordered table-hover"> 
+            <tr class="active"> 
+                <th>人员</th>
+                 
+                <th>本月使用</th>
+                 
+                <th>未归档</th>
+                 
+                <th>总计使用</th>
+                 
+                <th>总计归档</th>
+                 
+            </tr>
+              
+            <tr> 
+                <td>上官金红</td>
+                 
+                <td>4</td>
+                 
+                <td>15</td>
+                 
+                <td>95</td>
+                 
+                <td>80</td>
+                 
+            </tr>
+              
+            <tr> 
+                <th>合计</th>
+                 
+                <td>4</td>
+                 
+                <td>15</td>
+                 
+                <td>95</td>
+                 
+                <td>80</td>
+                 
+            </tr>
+             
+        </table>
+         
+    </div> 
+</div>
