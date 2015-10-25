fb5e00ad7e9e5400a3f062c298efbecbd0b9fa5b
丁一鸣 <315129552@qq.com>
2015-10-25 22:30:38 +0800

    修改数据库连接IP

--- app/Report/mark_count.php
+++ app/Report/mark_count.php
@@ -1,7 +1,7 @@
 ﻿<?php
    /* 厂商 Mark情况统计表 */
 //  header("Content-Type:text/html;charset=utf-8");
-  $mysqli = new mysqli('localhost','root','HWVosvmGYP6knCjp6Ihx','robot');
+  $mysqli = new mysqli('118.244.197.36','root','HWVosvmGYP6knCjp6Ihx','robot');
   
   /* check connection */
 	if (mysqli_connect_errno()) {
