2bde6487f4050a37529818ed2c60b23ee67aca5b
Aaronqcd <812159920@qq.com>
2015-10-29 23:17:27 +0800

    修改代理商首页，保持与原型一致(P6)

--- resources/views/page/home/agency.blade.php
+++ resources/views/page/home/agency.blade.php
@@ -15,10 +15,10 @@
 					<tr>
 						<th>提示信息</th>
 						<th>编号</th>
-						<th>状态</th>
+						<th>设备状态</th>
 						<th>销售状态</th>
 						<th>医院</th>
-						<th>代理商</th>
+						<!-- <th>代理商</th> -->
 						<th>负责人</th>
 						<th>维护记录</th>
 						<th></th>
@@ -30,12 +30,12 @@
 						<td>[:item.cust_id:]</td>
 						<td>[:SIns.robot_action_type[item.status].name:]</td>
 						<td>[:SIns.lease_type[item.lease_type_id].name:]</td>
-						<td>[:item.agency_name:]</td>
 						<td>[:item.hospital_name:]</td>
+						<!-- <td>[:item.agency_name:]</td> -->
 						<td>[:item.employee_name:]</td>
 						<td>[:item.log_count || 0:]</td>
 						<td class="text-center">
-							<a ui-sref="base.robot.detail({id : item.id})" title="" class="btn btn-sm btn-primary">查看</a>
+							<!-- <a ui-sref="base.robot.detail({id : item.id})" title="" class="btn btn-sm btn-primary">查看</a> -->
 						</td>
 					</tr>
 				</tbody>
