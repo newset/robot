a653049cf0a9886828467863299e4fd24929bc8c
Insane <luojieyy@gmail.com>
2015-11-05 09:32:27 +0800

    更改顺序

--- resources/views/page/robot/home.blade.php
+++ resources/views/page/robot/home.blade.php
@@ -25,6 +25,39 @@
 					</tr>
 				</thead>
 				<tbody>
+					<tr ng-repeat="item in data.e">
+						<td>[:info(item):]</td>
+						<td>[:item.cust_id:]</td>
+						<td>[:SIns.robot_action_type[item.status].name:]</td>
+						<td>[:SIns.lease_type[item.lease_type_id-1].name:]</td>
+						<td>[:item.hospital_name:]</td>
+						<td>[:item.agency_name:]</td>
+						<td>[:item.employee_name:]</td>
+						<td>[:item.log_count || 0:]</td>
+						<td class="text-center">
+							<a ui-sref="base.robot.detail({id : item.id})" title="" class="btn btn-sm btn-primary">查看</a>
+						</td>
+					</tr>
+					<tr ng-if="!data.e.length">
+						<td colspan="9" class="text-center">暂无相关数据</td>
+					</tr>
+				</tbody>
+			</table>
+			<table class="table table-bordered table-hover table-striped">
+				<thead>
+					<tr class="info">
+						<th>提示信息</th>
+						<th>编号</th>
+						<th>状态</th>
+						<th>销售状态</th>
+						<th>医院</th>
+						<th>代理商</th>
+						<th>负责人</th>
+						<th>维护记录</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
 					<tr ng-repeat="item in data.s">
 						<td>[:info(item):]</td>
 						<td>[:item.cust_id:]</td>
@@ -72,39 +105,6 @@
 						</td>
 					</tr>
 					<tr ng-if="!data.c.length">
-						<td colspan="9" class="text-center">暂无相关数据</td>
-					</tr>
-				</tbody>
-			</table>
-			<table class="table table-bordered table-hover table-striped">
-				<thead>
-					<tr class="info">
-						<th>提示信息</th>
-						<th>编号</th>
-						<th>状态</th>
-						<th>销售状态</th>
-						<th>医院</th>
-						<th>代理商</th>
-						<th>负责人</th>
-						<th>维护记录</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr ng-repeat="item in data.e">
-						<td>[:info(item):]</td>
-						<td>[:item.cust_id:]</td>
-						<td>[:SIns.robot_action_type[item.status].name:]</td>
-						<td>[:SIns.lease_type[item.lease_type_id-1].name:]</td>
-						<td>[:item.hospital_name:]</td>
-						<td>[:item.agency_name:]</td>
-						<td>[:item.employee_name:]</td>
-						<td>[:item.log_count || 0:]</td>
-						<td class="text-center">
-							<a ui-sref="base.robot.detail({id : item.id})" title="" class="btn btn-sm btn-primary">查看</a>
-						</td>
-					</tr>
-					<tr ng-if="!data.e.length">
 						<td colspan="9" class="text-center">暂无相关数据</td>
 					</tr>
 				</tbody>
