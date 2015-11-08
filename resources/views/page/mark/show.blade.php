fa2603029ac7dbdd7ef6814da4602683fd044d35
Aaronqcd <812159920@qq.com>
2015-11-08 23:54:26 +0800

    修改mark详情页面的按钮

--- resources/views/page/mark/show.blade.php
+++ resources/views/page/mark/show.blade.php
@@ -7,8 +7,8 @@
 				<button type="button" class="btn btn-primary no-animate" ng-click="bind()" ng-show="SIns.current_row.agency_id == -1">绑定</button>
 				<button type="button" class="btn btn-primary no-animate" ng-click="unbind()" ng-show="SIns.current_row.agency_id != -1">解绑</button>
 				
-				<button type="button" class="btn btn-primary" ng-click="recycle()">损坏报废</button>
-				<button type="button" class="btn btn-primary" ng-click="replace()">损坏更新</button>
+				<button type="button" class="btn btn-primary" ng-click="recycle()" ng-disabled="SIns.current_row.status != 1">损坏报废</button>
+				<button type="button" class="btn btn-primary" ng-click="replace()" ng-disabled="SIns.current_row.status != 1">损坏更新</button>
 			</div>
 			<div class="clearfix"></div>
 		</div>
