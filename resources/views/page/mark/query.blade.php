a3818e83217800cf16f661ad8facc35069b9935f
Aaronqcd <812159920@qq.com>
2015-11-24 00:18:16 +0800

    修复错误

--- resources/views/page/mark/query.blade.php
+++ resources/views/page/mark/query.blade.php
@@ -299,9 +299,8 @@
 									  <td>
 										  <span ng-if="row.status == 1">未使用</span>
 										  <span ng-if="row.status == 2">使用完毕</span>
-										  <span ng-if="row.status == 3">已绑定</span>
-										  <span ng-if="row.status == 4">损坏报废</span>
-										  <span ng-if="row.status == 5">损坏更换</span>
+										  <span ng-if="row.status == 3">损坏报废</span>
+										  <span ng-if="row.status == 4">损坏更换</span>
 									  </td>
 									  <td>[:row.hospital_name:]</td>
 									  <td>[:row.doctor_name || '-' :]</td>
