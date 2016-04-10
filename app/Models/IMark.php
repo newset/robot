9cf2311294ebcffec8f1fb12c49330ca719ee1e5
Aaronqin <Aaronqcd@gmail.com>
2016-04-10 19:17:20 +0800

    修改sql

--- app/Models/IMark.php
+++ app/Models/IMark.php
@@ -261,7 +261,7 @@ class IMark extends BaseModel
             $ins->where('agency_id',uid())
             ->where('doctor_id','>',0)
             ->whereNull('archive_at')
-            ->where(function($query)
+            ->where(function($query, $time)
             {
                 $query->where(DB::raw('CAST(used_at as datetime)') ,'<=',$time)
                       ->orWhereNull('used_at');
